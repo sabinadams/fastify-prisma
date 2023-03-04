d95e965139a1988f6750518c5d6ac44cb6c6dbb4
Sabin Adams <sabintheworld@gmail.com>
2023-03-03 22:25:28 -0800

    Prettier config

--- example/index.ts
+++ example/index.ts
@@ -3,13 +3,16 @@ import prismaPlugin from '../dist'
 
 const fastify = Fastify()
 
-fastify.register(prismaPlugin)
-
-fastify.get('/', async () => {
-	const users = await fastify.prisma.user.findMany()
-	return { users }
+fastify.register(prismaPlugin, {
+  log: ['info'],
 })
 
-fastify.listen({ port: 3000 }, (err, address) => {
-	console.info(`⚡️ Server running at ${address}`)
+fastify.get('/', async () => {
+  const users = await fastify.prisma.user.findMany()
+  return { users }
+})
+
+fastify.listen({ port: 3000 }, async (err, address) => {
+  console.info(`⚡️ Server running at ${address}`)
+  await fastify.prisma.$on('info', (e) => console.log(e))
 })
--- index.ts
+++ index.ts
@@ -4,23 +4,25 @@ import { PrismaClient } from '@prisma/client'
 import { PrismaClientOptions } from '@prisma/client/runtime'
 
 declare module 'fastify' {
-	interface FastifyInstance {
-		prisma: PrismaClient
-	}
+  interface FastifyInstance {
+    prisma: PrismaClient
+  }
 }
 
-const prismaPlugin: FastifyPluginAsync<Omit<PrismaClientOptions, '__internal'>> = async (fastify, options) => {
-	if (!fastify.hasDecorator('prisma')) {
-		const prisma = new PrismaClient(options)
-		await prisma.$connect()
-		fastify
-			.decorate('prisma', new PrismaClient(options))
-			.addHook('onClose', async (server) => {
-				await server.prisma.$disconnect()
-			})
-	} else {
-		throw new Error('The `prisma` decorator has already been registered.')
-	}
+const prismaPlugin: FastifyPluginAsync<
+  Omit<PrismaClientOptions, '__internal'>
+> = async (fastify, options) => {
+  if (!fastify.hasDecorator('prisma')) {
+    const prisma = new PrismaClient(options)
+    await prisma.$connect()
+    fastify
+      .decorate('prisma', new PrismaClient(options))
+      .addHook('onClose', async (server) => {
+        await server.prisma.$disconnect()
+      })
+  } else {
+    throw new Error('The `prisma` decorator has already been registered.')
+  }
 }
 
 export default fp(prismaPlugin)
